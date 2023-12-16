c8d5517a562f86606b5e652b6c0294fadd4254bb
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-16 17:57:33 +0300

    Add useEffect with onScroll function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,80 @@
+import { useState, useEffect } from 'react'
 import { FaTwitter, FaFacebook, FaInstagram } from 'react-icons/fa'
 
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState('home')
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true)
+      }else{
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener("scroll", onScroll)
+
+    return () => window.removeEventListener("scroll", onScroll)
+  }, [])
+
   return (
-    <nav className='m-2'>
-      <div className='flex gap-2 justify-between'>
-        <div href="/">
-          <img src={''} alt="" />
-        </div>
+    <nav className='m-2 flex justify-between'>
+      <div href="/">
+        <img src={''} alt="" />
+      </div>
 
+      <div>
+        <a 
+          href="#home"
+          className='mr-2'
+        >
+          Home
+        </a>
+        <a 
+          href="#skills"
+          className='mr-2'
+        >
+          Skills
+        </a>
+        <a 
+          href="#projects"
+        >
+          Projects
+        </a>
+      </div>
+
+      <div className='flex gap-2'>
+        <div className='flex gap-2 items-center'>
+          <div>
+            <a 
+              href='#'
+            >
+              <FaFacebook />
+            </a>
+          </div>
+          <div>
+            <a 
+              href='#'
+            >
+              <FaTwitter />
+            </a>
+          </div>
+          <div>
+            <a 
+              href='#'
+            >
+              <FaInstagram />
+            </a>
+          </div>
+        </div>
         <div>
-          <a 
-            href="#home"
-            className='mr-2'
+          <button
+            onClick={() => console.log('connect')}
           >
-            Home
-          </a>
-          <a 
-            href="#skills"
-            className='mr-2'
-          >
-            Skills
-          </a>
-          <a 
-            href="#projects"
-          >
-            Projects
-          </a>
-        </div>
-
-        <div className='flex gap-2'>
-          <a 
-            href='#'
-          >
-            <FaFacebook />
-          </a>
-          <a 
-            href='#'
-          >
-            <FaTwitter />
-          </a>
-          <a 
-            href='#'
-          >
-            <FaInstagram />
-          </a>
+            Let's Connect
+          </button>
         </div>
       </div>
     </nav>
