2360b24885e993b732d06f679a4ef5e5be003927
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-20 15:49:08 +0300

    Add ProjectCard component to be used in the Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,8 @@ import pearlsCollections from "../assets/img/pearlsCollections.PNG"
 import airbnb from "../assets/img/airbnb.PNG"
 import swimmingBlog from "../assets/img/swimmingBlog.PNG"
 import { Col, Container, Row, Nav, Tab } from "react-bootstrap"
+import { ProjectCard } from "./ProjectCard"
+import colorSharp2 from "../assets/img/color-sharp2.png"
 
 export const Projects = () => {
   const projects = [
@@ -29,15 +31,9 @@ export const Projects = () => {
           <Col>
             <h2>Projects</h2>
             <Tab.Container id="projects-tabs" defaultActiveKey="first">
-              <Nav variant="pills" className="flex-column">
+              <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                 <Nav.Item>
                   <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
                 </Nav.Item>
               </Nav>
 
@@ -47,18 +43,25 @@ export const Projects = () => {
                     {
                       projects.map((project, index) => {
                         return (
-                          <p>{project.title}</p>
+                          <ProjectCard
+                            key={index}
+                            {...project}
+                          />
                         )
                       })
                     }
                   </Row>
                 </Tab.Pane>
-                <Tab.Pane eventKey="second">Second tab content</Tab.Pane>
               </Tab.Content>
             </Tab.Container>
           </Col>
         </Row>
       </Container>
+
+      <img 
+        className="background-image-right"
+        src={colorSharp2}
+      />
     </section>
   )
 }
