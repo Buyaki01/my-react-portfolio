12969ad7c9b64571c05775297609520388d3eea9
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-20 10:40:46 +0300

    Install react-multi-carousel, Add Skills component and it's styling, update Banner component

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,56 +1,17 @@
 import { Col, Container, Row } from "react-bootstrap"
 import { ArrowRightCircle } from "react-bootstrap-icons"
-import headerImg from "../assets/img/header-img.svg"
-import { useEffect, useState } from "react"
+import Image from 'react-bootstrap/Image'
+import professionalPhoto from "../assets/img/professionalPhoto.jpg"
 
 export const Banner = () => {
-  const [loopNum, setLoopNum] = useState(0)
-  const [isDeleting, setIsDeleting] = useState(false)
-  const toRotate = ["Front-End Developer", "Back-End Developer", "Full-Stack Developer", "MERN Stack Developer"]
-  const [text, setText] = useState('')
-  const [index, setIndex] = useState(1)
-  const [delta, setDelta] = useState(300 - Math.random() * 100)
-  const period = 2000
-
-  useEffect(() => {
-    let ticker = setInterval(() => {
-      tick()
-    }, delta)
-
-    return () => { clearInterval(ticker)}
-  }, [text])
-
-  const tick = () => {
-    let i = loopNum % toRotate.length
-    let fullText = toRotate[i]
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
-
-    setText(updatedText)
-
-    if (isDeleting) {
-      setDelta(prevDelta => prevDelta/2)
-    }
-
-    if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true)
-      setDelta(period)
-    } else if(!isDeleting && updatedText === '') {
-      setIsDeleting(false)
-      setLoopNum(loopNum + 1)
-      setDelta(500)
-    } else {
-      setIndex(prevIndex => prevIndex + 1)
-    }
-  }
-
   return (
     <section className="banner" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
             <span className="tagline">Welcome to my Portfolio</span>
-            <h1>{`Hi I'm Ritta Sweta `}<span className="wrap">{text}</span></h1>
-            <p>With experience in JavaScript, React, NextJS, MERN Stack</p>
+            <h1>{`Hi I'm Ritta Sweta, `}<span className="wrap">Full-Stack Web Developer</span></h1>
+            <p>With experience in JavaScript, React, NextJS, NodeJs, MERN Stack</p>
             <button 
               onClick={() => console.log('connect')}
             >
@@ -59,7 +20,9 @@ export const Banner = () => {
           </Col>
 
           <Col xs={12} md={6} xl={5}>
-            <img src={headerImg} alt="Header Img" />
+            <div class="professionalPhotoContainer">
+              <Image src={professionalPhoto} alt="Professional Photo" thumbnail class="professionalPhoto"/>
+            </div>
           </Col>
         </Row>
       </Container>
